3f18c5ace2efbcae0afc83b78e5b5827fac1a128
Simon Miner <simon.miner@gmail.com>
2023-06-23 21:47:31 -0400

    Add methods to SoundPointer class

--- src/sound-pointer.js
+++ src/sound-pointer.js
@@ -11,36 +11,45 @@ export class SoundPointer {
  
     minFrequency = 220;
     maxFrequency = 880;
-    frequencyCount = 0;
-    //frequencyCount = maxFrequency - minFrequency;
 
     maxLeftPan = -1;
     maxRightPan = 1;
-    panWidth = 0;
-    // panWidth = maxRightPan - maxLeftPan;
 
     minVolume = 0.1;
     maxVolume = 0.6;
-    volumeRange = 0;
-    // volumeRange = maxVolume - minVolume;
 
     /**
      * Sound type; may be one of sine (the default), square, sawtooth, or triangle.
-     * @tpe {String}
+     * @type {String}
      */
     soundType = "sine";
 
-    /**
-     * Pizzicato object used to generate sound.
-     * @type {Pizzicato}
-     */
-    sound;
+    
 
     /**
      * Is the sound currently playing?
      * @type {boolean}
      */
     isSoundPlaying = false;
+
+    /**
+     * Current X and Y coordinates of the mouse.
+     * @type {Object}
+     */
+    mousePosition = { x: undefined, y: undefined };
+
+    /**
+     * Pizzicato.Sound object used to generate sound.
+     * @type {Pizzicato.Sound}
+     */
+    sound;
+
+    /**
+     * @returns {SoundPointer}
+     */
+    constructor() {
+        this.sound = this.buildSound();
+    }
 
     /**
      * Build the sound to be played using the
@@ -67,4 +76,83 @@ export class SoundPointer {
 
         return sound;
     }
+
+    /**
+     * Computes the left/right pan value for the current
+     * horizontal position of the mouse pointer.
+     * @param {int} horizontalPosition X-position in pixels
+     * @returns {float}
+     */
+    getCurrentPan(horizontalPosition) {
+        const width = window.innerWidth;
+        const pan = horizontalPosition / width * (this.maxRightPan - this.maxLeftPan) + this.maxLeftPan;
+        return parseFloat(pan.toFixed(3));
+    }
+
+    /**
+     * Computes the volume level for the current
+     * horizontal position of the mouse pointer.
+     * @param {int} horizontalPosition X-position in pixels
+     * @returns {float}
+     */
+    getCurrentVolume(horizontalPosition) {
+        const width = window.innerWidth;
+        const volume = horizontalPosition / width * (this.maxVolume - this.minVolume) + this.minVolume;
+        return parseFloat(volume.toFixed(3));
+    }
+ 
+    /**
+     * Computes the sound frequency for the current
+     * vertical position of the mouse pointer.
+     * @param {int} verticalPosition Y-position in pixels
+     * @returns {int}
+     */
+    getCurrentFrequency(verticalPosition) {
+        const height = window.innerHeight;
+        // Pixels count from top to bottom of browser window,
+        // so we need to reverse the ratio.
+        const frequency =
+            ((height - verticalPosition) / height * (this.maxFrequency - this.minFrequency)) 
+            + this.minFrequency;
+        return parseInt(frequency);
+    }
+
+    /**
+     * Toggles the current state of the sound from playing to
+     * stopped or from stopped to playing. Returns a boolean
+     * value indicating whether or not the sound is playing.
+     * @returns {boolean}
+     */
+    toggleSound() {
+        this.isSoundPlaying ? this.sound.pause() : this.sound.play();
+        this.isSoundPlaying = !this.isSoundPlaying;
+        return this.isSoundPlaying;
+    };
+
+    /**
+     * Updates the pan position, volume, and pitch of the
+     * sound based on the X and Y mouse pointer coordinates.
+     * Returns the updated mouse position as an associative array
+     * with "X" and "Y" entries.
+     * @param {int} horizontalPosition X-position in pixels
+     * @param {int} verticalPosition Y-position in pixels
+     * @returns {Object}
+     */
+    updateSoundForMousePosition(horizontalPosition, verticalPosition) {
+        this.mousePosition = {x: horizontalPosition, y: verticalPosition};
+
+        // Adjust the pan and volume of the sound based on
+        // the horizontal position of the mouse pointer.
+        const pan = this.getCurrentPan(this.mousePosition.x);
+        this.sound.effects[0].pan = pan;
+        const volume = this.getCurrentVolume(this.mousePosition.x);
+        this.sound.volume = volume;
+        
+        // Adjust the pitch of the sound based on
+       // the vertical position of the mouse pointer.
+        const frequency = this.getCurrentFrequency(this.mousePosition.y);
+        sound.frequency = frequency;
+        
+        return this.mousePosition;
+    };
 }
